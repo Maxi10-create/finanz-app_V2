5421563512995d05ae8ac2ed4bc6fa5e2aac46c8
Maxi10-create <hofermaximilian@icloud.com>
2026-04-23 15:48:03 +0200

    Update API_BASE_URLS in config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   API_BASE_URLS: [
-    'https://script.google.com/macros/s/AKfycbwS2ZsKXARVVZeyM3TKiGmjewYJ-ROc2npBFTxFy6vUp-Med8RsPdwC6FwApmvuuMbvCg/exec'
+    'https://script.google.com/macros/s/AKfycbxPG9El1qkdYGHWDwKsVV5njaE09WfN7yQHT4OB-TYhjIQAS_ZgEWHv4G0FhHsc6pnY-g/exec'
   ],
   USER_NAME: 'Hofer Maximilian',
   OTHER_PERSON: 'Jana March',
